f180571fb60d3fc5335938a7d1ef23531ef4c45a
LKS007 <leon99_21@mail.ru>
2016-06-15 12:49:26 +0300

    add wrapper on the _form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to @task
+      redirect_to task_path(@task)
     else
       render 'edit'
     end
