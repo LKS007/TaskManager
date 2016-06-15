cb226e9fc9e76731ec347bd21808b9c941fafe69
LKS007 <leon99_21@mail.ru>
2016-06-15 13:19:48 +0300

    delete assets

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   private
-    def task_params
-      params.require(:task).permit(:title, :text)
-    end
+  def task_params
+    params.require(:task).permit(:title, :text)
+  end
 end		
